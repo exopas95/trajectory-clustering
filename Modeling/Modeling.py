42b0434a773e37a69dcac0cff79eaecb9d88784d
Tree-Nulbo <exopas58@gmail.com>
2019-06-14 04:02:30 +0900

    Finalize Code

--- Modeling/Modeling.py
+++ Modeling/Modeling.py
@@ -6,11 +6,6 @@ from shapely.geometry import MultiPoint
 from sklearn.cluster import DBSCAN
 from geopy.distance import great_circle
 
-####################################
-## functions below are taken (and modified a bit) from blog post published by Geoff Boeing
-## http://geoffboeing.com/2014/08/clustering-to-reduce-spatial-data-set-size/
-####################################
-
 def getDbScanClustersCenters(df, epsInKm, minObjects):
     clusters = getDbScanClusters(df, epsInKm, minObjects)
     return getClustersCenters(clusters)
@@ -36,11 +31,13 @@ def getClustersCenters(clusters):
 
 ####################################
 
+# 노드를 맵에 표시하는 코드
 def drawPointsOnMap(superMap, points, color, radiusSize):
     print('Drawing {} points on map'.format(len(points)))
     for point in points:
         folium.CircleMarker(point, radius = radiusSize, fill_color = color, color = color).add_to(superMap)
         
+# 두 노드간의 거리를 측정하는 코드
 def getDistanceBetweenPoints(point1, point2):
         lon1 = point1[1] * pi / 180.0
         lon2 = point2[1] * pi / 180.0
@@ -54,7 +51,8 @@ def getDistanceBetweenPoints(point1, point2):
         c = 2.0 * arctan2(sqrt(a), sqrt(1.0-a))
         km = 6371.0 * c
         return km
-    
+
+# 경로의 종단점을 설정하는 코드
 def getEndPoint(points):
     pointsForFindingLineEnd = list(points)
     currentPoint = random.choice(pointsForFindingLineEnd)
@@ -75,6 +73,7 @@ def getEndPoint(points):
     
     return currentPoint
 
+# 노드와 노드를 연결하는 코드
 def getLineFromPoints(points, startingPoint):
     linePoints = list(points)
     currentPoint = startingPoint
@@ -95,26 +94,31 @@ def getLineFromPoints(points, startingPoint):
     return line
 
 for i in range(1, 6):
-    df = pd.read_csv('C:\\Users\\cross\\Desktop\\DM\\NewDataset\\dataset' + str(i) + '.csv', index_col = False, header=0)
+    # CSV를 읽어온다 - 다른 컴퓨터에서 사용할 경우 경로를 수정해주어야 한다.
+    df = pd.read_csv('C:\\Users\\Sewoong\\Desktop\\Develop\\Trajectory-Clustering\\NewDatasetA\\dataset' + str(i) + '.csv', index_col = False, header=0)
     superMap = folium.Map(location=[37.242837, 127.080046], zoom_start=20, tiles='OpenStreetMap')
-    #drawPointsOnMap(superMap, df.values, '#3186cc', 0.1)
+    # 1- 모든 노드를 맵에 표시하는 코드
+    drawPointsOnMap(superMap, df.values, '#3186cc', 0.1)
 
-    clustersCenters = getDbScanClustersCenters(df, 0.002, 0.1)
-    drawPointsOnMap(superMap, clustersCenters, '#ff0000', 10)
-
+    # 2- 클러스터링 한 이후 클러스터를 맵에 표시하는 코드
+    clustersCenters = getDbScanClustersCenters(df, 0.008, 0.1)
+    drawPointsOnMap(superMap, clustersCenters, '#ff0000', 1)
+    
+    # 3- 경로를 맵에 표시하는 코드
     endPoint = getEndPoint(clustersCenters)
-    drawPointsOnMap(superMap, [endPoint], '#3186cc', 1)
+    drawPointsOnMap(superMap, [endPoint], '#3186cc', 3)
     line = getLineFromPoints(clustersCenters, endPoint)
 
-    superMap.add_child(folium.PolyLine(locations = line, weight = 2, color="#d63b3b", ))
+    superMap.add_child(folium.PolyLine(locations = line, weight = 5, color="#d63b3b", ))
     
+    # 클러스터링된 결과를 저장할 경로를 설정해준다 - 다른 컴퓨터에서 사용할 경우 경로를 수정해주어야 한다.
     if(i == 1):
-        superMap.save('C:\\Users\\cross\\Desktop\\DM\\ClusteringResults\\Monday.html')
+        superMap.save('C:\\Users\\Sewoong\\Desktop\\Develop\\Trajectory-Clustering\\ClusteringResults\\MondayA_Trajectory.html')
     elif(i == 2): 
-        superMap.save('C:\\Users\\cross\\Desktop\\DM\\ClusteringResults\\Tuesday.html')
+        superMap.save('C:\\Users\\Sewoong\\Desktop\\Develop\\Trajectory-Clustering\\ClusteringResults\\TuesdayA_Trajectory.html')
     elif(i == 3):
-        superMap.save('C:\\Users\\cross\\Desktop\\DM\\ClusteringResults\\Wendsday.html')
+        superMap.save('C:\\Users\\Sewoong\\Desktop\\Develop\\Trajectory-Clustering\\ClusteringResults\\WendsdayA_Trajectory.html')
     elif(i == 4):
-        superMap.save('C:\\Users\\cross\\Desktop\\DM\\ClusteringResults\\Thursday.html')
+        superMap.save('C:\\Users\\Sewoong\\Desktop\\Develop\\Trajectory-Clustering\\ClusteringResults\\ThursdayA_Trajectory.html')
     elif(i == 5):
-        superMap.save('C:\\Users\\cross\\Desktop\\DM\\ClusteringResults\\Friday.html')
+        superMap.save('C:\\Users\\Sewoong\\Desktop\\Develop\\Trajectory-Clustering\\ClusteringResults\\FridayA_Trajectory.html')
--- Preprocessing.R
+++ Preprocessing.R
@@ -3,9 +3,9 @@
 
 library(dplyr)
 library(readr)
-#setwd(ê²½ë¡œì„¤ì •)
+#setwd(°æ·Î¼³Á¤)
 
-dir <- ("C:/Users/cross/Desktop/DM/Dataset")
+dir <- ("C:/Users/Sewoong/Desktop/Develop/Trajectory-Clustering/DatasetB")
 file_list <- list.files(dir)
 file_list
 lat <- c()
@@ -16,73 +16,86 @@ month <- c()
 day <- c()
 hour <- c()
 weekdays <- c()
-data <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
+data1 <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
+data2 <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
+data3 <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
+data4 <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
+data5 <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
+
+FinalData1 <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
+FinalData2 <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
+FinalData3 <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
+FinalData4 <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
+FinalData5 <- data.frame(lat, lon, ele, time, month, day, hour, weekdays)
 num = 0
 
 for(file in file_list){
   print(file)
   temp <- read.csv(paste(dir, file, sep ="\\"),header=TRUE, sep=",",stringsAsFactors=FALSE)
   att <- names(temp)
-
+  
   for (name in att){
-    if(name == "lat"){
-      temp <- rename(temp, ele = ns1.ele)
-      temp <- rename(temp, time = ns1.time)
-      newTemp <- select(temp,c(lat,lon, ele, time))
-      
-      ### í¡ì—°ì¥ì˜ ê³ ë„ : 84~85 ### 
-      ###ê³µëŒ€ì˜ 1ì¸µ ê³ ë„ : 89~90 ###
-      ###ë©€ê´€ì˜ 2ì¸µ ê³ ë„ :68.09(í‰ê· ì‚°ì¶œ ê²°ê³¼) -> ë©€ê´€ 1ì¸µì€ 66ì •ë„? ###
-      ###ìš°ì •ì›ì˜ ì¸µì˜ ê³ ë„ : 74ì´í•˜ ###
-      #êµì™¸/ë©€ê´€/ê³µëŒ€ ì œì™¸í•˜ëŠ” ì½”ë“œ
-      newTemp <- newTemp %>% filter(newTemp$lat<=37.2480, newTemp$lat>= 37.2380 & newTemp$lon <=127.086,newTemp$lon>=127.0757 )
-      newTemp <- newTemp %>% filter(newTemp$lat<37.2449 | newTemp$lon<127.07857 | newTemp$ele<85)
-      newTemp <- newTemp %>% filter(newTemp$ele < 67 | newTemp$lat>37.24481 | newTemp$lat <37.243981 | newTemp$lon >127.076936 | newTemp$lon <127.075835) 
-      
-      ### ì‹œê°„ì¢Œí‘œ ì „ì²˜ë¦¬ tzone ê¸°ëŠ¥ì‚¬ìš© ###
-      newTemp$time<- as.POSIXct(newTemp$time)
-      attributes(newTemp$time)$tzone <- "Asia/Seoul"
-      newTemp$month <- as.numeric(format(newTemp$time, '%m'))
-      newTemp$day <- as.numeric(format(newtemp$time, '%d'))
-      newTemp$hour <- as.numeric(format(newTemp$time, '%H'))
-      newTemp$weekdays <- weekdays.POSIXt(newTemp$time,abbreviate=T)
-      
-      
-      data <- rbind(data,newTemp)
-      data <- data[-1,]
-      write.csv(data, paste0("C:/Users/cross/Desktop/DM/NewDataset/dataset", num, ".csv"))
-      num = num + 1
-    }
-    else if(name == "Latitude"){
+    if(name == "Latitude"){
       temp <- rename(temp, lat = Latitude)
       temp <- rename(temp, lon = Longitude)
       temp <- rename(temp, ele = Elevation)
       temp <- rename(temp, time = Timestamp)
       newTemp <- select(temp,c(lat,lon, ele, time))
-      
-      ### í¡ì—°ì¥ì˜ ê³ ë„ : 84~85 ### 
-      ###ê³µëŒ€ì˜ 1ì¸µ ê³ ë„ : 89~90 ###
-      ###ë©€ê´€ì˜ 2ì¸µ ê³ ë„ :68.09(í‰ê· ì‚°ì¶œ ê²°ê³¼) -> ë©€ê´€ 1ì¸µì€ 66ì •ë„? ###
-      ###ìš°ì •ì›ì˜ ì¸µì˜ ê³ ë„ : 74ì´í•˜ ###
-      #êµì™¸/ë©€ê´€/ê³µëŒ€ ì œì™¸í•˜ëŠ” ì½”ë“œ
-      newTemp <- newTemp %>% filter(newTemp$lat<=37.2480, newTemp$lat>= 37.2380 & newTemp$lon <=127.086,newTemp$lon>=127.0757 )
-      newTemp <- newTemp %>% filter(newTemp$lat<37.2449 | newTemp$lon<127.07857 | newTemp$ele<85)
-      newTemp <- newTemp %>% filter(newTemp$ele < 67 | newTemp$lat>37.24481 | newTemp$lat <37.243981 | newTemp$lon >127.076936 | newTemp$lon <127.075835) 
-      
-      ### ì‹œê°„ì¢Œí‘œ ì „ì²˜ë¦¬ tzone ê¸°ëŠ¥ì‚¬ìš© ###
-      newTemp$time<- as.POSIXct(newTemp$time)
-      attributes(newTemp$time)$tzone <- "Asia/Seoul"
-      newTemp$month <- as.numeric(format(newTemp$time, '%m'))
-      newTemp$day <- as.numeric(format(newTemp$time, '%d'))
-      newTemp$hour <- as.numeric(format(newTemp$time, '%H'))
-      newTemp$weekdays <- weekdays.POSIXt(newTemp$time,abbreviate=T)
-
-
-      data <- rbind(data,newTemp)
-      data <- data[-1,]
-      write.csv(data, paste0("C:/Users/cross/Desktop/DM/NewDataset/dataset", num, ".csv"))
-      num = num + 1
+    }
+    if(name == "lan"){
+      temp <- rename(temp, ele = ns1.ele)
+      temp <- rename(temp, time = ns1.time)
+      newTemp <- select(temp,c(lat,lon, ele, time))
     }
   }
+      
+    ### Èí¿¬ÀåÀÇ °íµµ : 84~85 ### 
+    ###°ø´ëÀÇ 1Ãş °íµµ : 89~90 ###
+    ###¸Ö°üÀÇ 2Ãş °íµµ :68.09(Æò±Õ»êÃâ °á°ú) -> ¸Ö°ü 1ÃşÀº 66Á¤µµ? ###
+    ###¿ìÁ¤¿øÀÇ ÃşÀÇ °íµµ : 74ÀÌÇÏ ###
+    #±³¿Ü/°ø´ë¿Ü°¢/µµ¼­°ü¿Ü°¢/±¹Á¦ÇĞ°ü¾Æ·¡ Á¦¿ÜÇÏ´Â ÄÚµå
+    newTemp <- newTemp %>% filter(newTemp$lat <= 37.2480, newTemp$lat>= 37.2380 & newTemp$lon <=127.086,newTemp$lon>=127.0757 )
+    newTemp <- newTemp %>% filter(newTemp$lat < 37.242607 | newTemp$lon < 127.081834)
+    newTemp <- newTemp %>% filter(newTemp$lat > 37.240400 | newTemp$lon > 127.079036)
+    newTemp <- newTemp %>% filter(newTemp$lat > 37.240156)
+      
+      
+    ### ½Ã°£ÁÂÇ¥ ÀüÃ³¸® tzone ±â´É»ç¿ë ###
+    newTemp$time<- as.POSIXct(newTemp$time)
+    attributes(newTemp$time)$tzone <- "Asia/Seoul"
+    newTemp$month <- as.numeric(format(newTemp$time, '%m'))
+    newTemp$day <- as.numeric(format(newTemp$time, '%d'))
+    newTemp$hour <- as.numeric(format(newTemp$time, '%H'))
+    newTemp$weekdays <- weekdays.POSIXt(newTemp$time,abbreviate=T)
+      
+    data1 <- newTemp %>% filter(newTemp$weekdays == '¿ù')
+    data2 <- newTemp %>% filter(newTemp$weekdays == 'È­')
+    data3 <- newTemp %>% filter(newTemp$weekdays == '¼ö')
+    data4 <- newTemp %>% filter(newTemp$weekdays == '¸ñ')
+    data5 <- newTemp %>% filter(newTemp$weekdays == '±İ')
+      
+    FinalData1 <- rbind(FinalData1, data1)
+    FinalData2 <- rbind(FinalData2, data2)
+    FinalData3 <- rbind(FinalData3, data3)
+    FinalData4 <- rbind(FinalData4, data4)
+    FinalData5 <- rbind(FinalData5, data5)
 }
+
+FinalData1 <- FinalData1[-1,]
+FinalData2 <- FinalData2[-1,]
+FinalData3 <- FinalData3[-1,]
+FinalData4 <- FinalData4[-1,]
+FinalData5 <- FinalData5[-1,]
+
+FinalData1 <- FinalData1 %>% select(lat, lon)
+FinalData2 <- FinalData2 %>% select(lat, lon)
+FinalData3 <- FinalData3 %>% select(lat, lon)
+FinalData4 <- FinalData4 %>% select(lat, lon)
+FinalData5 <- FinalData5 %>% select(lat, lon)
+
+write.csv(FinalData1, paste0("C:/Users/Sewoong/Desktop/Develop/Trajectory-Clustering/NewDatasetB/dataset1.csv"))
+write.csv(FinalData2, paste0("C:/Users/Sewoong/Desktop/Develop/Trajectory-Clustering/NewDatasetB/dataset2.csv"))
+write.csv(FinalData3, paste0("C:/Users/Sewoong/Desktop/Develop/Trajectory-Clustering/NewDatasetB/dataset3.csv"))
+write.csv(FinalData4, paste0("C:/Users/Sewoong/Desktop/Develop/Trajectory-Clustering/NewDatasetB/dataset4.csv"))
+write.csv(FinalData5, paste0("C:/Users/Sewoong/Desktop/Develop/Trajectory-Clustering/NewDatasetB/dataset5.csv"))
 
